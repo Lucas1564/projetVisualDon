4a3d7fb4504215b4cd2546463fdc21559dbe2b56
Eloi Coquoz <eloi.coquoz@bluewin.ch>
2022-03-18 11:12:58 +0100

    Data Load

--- src/index.js
+++ src/index.js
@@ -3,9 +3,4 @@ import csv1 from '../assets/data/Medalists.csv';
 import csv2 from '../assets/data/Medals_Standing.csv';
 import csv3 from '../assets/data/Olympic Schedule.csv';
 
-d3.csv(csv1, function(data) {
-  for (var i = 0; i < data.length; i++) {
-    alert(data[i].NAME);
-    alert(data[i].SPORTS);
-  }
-});
+console.log(csv1);
