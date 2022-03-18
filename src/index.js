5b719acef3d1e44e959348b377c07adf4afed708
Eloi Coquoz <eloi.coquoz@bluewin.ch>
2022-03-18 10:27:29 +0100

    Add Data - D3

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import * as d3 from 'd3';
+import csv1 from '../assets/data/Medalists.csv';
+import csv2 from '../assets/data/Medals_Standing.csv';
+import csv3 from '../assets/data/Olympic Schedule.csv';
+
+d3.csv(csv1, function(data) {
+  for (var i = 0; i < data.length; i++) {
+    alert(data[i].NAME);
+    alert(data[i].SPORTS);
+  }
+});
