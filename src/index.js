c053a39d43222d1e29a5780a4948624e78a79ab6
Eloi Coquoz <eloi.coquoz@bluewin.ch>
2022-03-29 11:29:03 +0200

    Routeur / Calendrier

--- src/index.js
+++ src/index.js
@@ -33,6 +33,55 @@ for (var i = 0; i < pays.length; i++) {
   console.log(pays[i].NOC + " " + pays[i].Gold)
 }
 
+// Les tags dont nous avons besoin pour afficher le calendrier
+const calist = document.querySelector('.cal-list')
+const calListItemTemplate = document.querySelector('#cal-list-item-template')
+
 for (var i = 0; i < agenda.length; i++) {
-  console.log(agenda[i])
+  var cal = (Object.keys(agenda[i]).length) - 2;
 }
+for (var j = 1; j < cal; j++) {
+  var day = "Day " + j;
+  const newCal = calListItemTemplate.content.cloneNode(true) // true pour cloner également les enfants du node
+  // On modifie le lien pour lui mettre un href du style "#cal-id"
+  newCal.querySelector('a').href = '#calendrier-' + j
+  newCal.querySelector('.cal-list-item-title').innerText = day
+  calist.append(newCal)
+}
+
+
+
+// Affichage d'une section
+function displaySection() {
+  // S'il n'y a pas de hash (par ex, on est sur "localhost:8080/"), le défaut devient '#home'
+  const section = window.location.hash || '#home'
+  const sectionSplit = section.split('-')
+
+
+  // Chargement des éléments custom par section
+  switch (sectionSplit[0]) {
+    case '#calendrier':
+      if (sectionSplit[1]) {
+        alert("day" + sectionSplit[1])
+      } else {
+        alert("Calendrier")
+      }
+      break;
+    case '#athlete':
+      if (sectionSplit[1]) {
+        alert("athlete" + sectionSplit[1])
+      } else {
+        alert("athletes")
+      }
+      break;
+    case '#home':
+      alert("Home")
+      break;
+  }
+}
+
+// On link la fonction "displaySection" à l'événement hashchange pour être averti d'un changement de hash dans l'url
+window.addEventListener('hashchange', displaySection)
+
+// Affichage au chargement pour traiter l'url en cours (exemple: on ouvre un lien dans un nouvel onglet)
+displaySection()
