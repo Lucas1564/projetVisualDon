35003d1afea2faec8f7d9ac595d8543bdba80780
Eloi Coquoz <eloi.coquoz@bluewin.ch>
2022-04-25 13:22:43 +0200

    Add Event

--- src/sections/cal.js
+++ src/sections/cal.js
@@ -1,11 +1,19 @@
 import agenda from '../../assets/data/Olympic Schedule.csv';
+import medal from '../../assets/data/Medalists.csv';
 
 // Les tags dont nous avons besoin pour afficher le calendrier
 const datlist = document.querySelector('.date-list')
 const dateListItemTemplate = document.querySelector('#date-list-item-template')
 
+// Les tags dont nous avons besoin pour afficher les events
+const eventlist = document.querySelector('.event-list')
+const eventListItemTemplate = document.querySelector('#event-list-item-template')
+
+
+
 // Affiche le jour désiré dans la liste
 function renderCal(day) {
+  affichesSport();
   var dateDay = "2 Feb 2022";
   switch (day) {
     case "1":
@@ -77,97 +85,153 @@ function renderCal(day) {
     switch (dayNumber) {
       case 2:
         if (agenda[i].day2 !== '-') {
-          alert(agenda[i].Ceremonies);
+          var paragraph = newDate.querySelector('.event-list-item-title');
+          var text = document.createTextNode(agenda[i].Ceremonies + "\n");
+          paragraph.appendChild(text);
+          afficheSport(agenda[i].Ceremonies);
         }
         break;
       case 3:
         if (agenda[i].day3 !== '-') {
-          alert(agenda[i].Ceremonies);
+          var paragraph = newDate.querySelector('.event-list-item-title');
+          var text = document.createTextNode(agenda[i].Ceremonies + "\n");
+          paragraph.appendChild(text);
+          afficheSport(agenda[i].Ceremonies);
         }
         break;
       case 4:
         if (agenda[i].day4 !== '-') {
-          alert(agenda[i].Ceremonies);
+          var paragraph = newDate.querySelector('.event-list-item-title');
+          var text = document.createTextNode(agenda[i].Ceremonies + "\n");
+          paragraph.appendChild(text);
+          afficheSport(agenda[i].Ceremonies);
         }
         break;
       case 5:
         if (agenda[i].day5 !== '-') {
-          alert(agenda[i].Ceremonies);
+          var paragraph = newDate.querySelector('.event-list-item-title');
+          var text = document.createTextNode(agenda[i].Ceremonies + "\n");
+          paragraph.appendChild(text);
+          afficheSport(agenda[i].Ceremonies);
         }
         break;
       case 6:
         if (agenda[i].day6 !== '-') {
-          alert(agenda[i].Ceremonies);
+          var paragraph = newDate.querySelector('.event-list-item-title');
+          var text = document.createTextNode(agenda[i].Ceremonies + "\n");
+          paragraph.appendChild(text);
+          afficheSport(agenda[i].Ceremonies);
         }
         break;
       case 7:
         if (agenda[i].day7 !== '-') {
-          alert(agenda[i].Ceremonies);
+          var paragraph = newDate.querySelector('.event-list-item-title');
+          var text = document.createTextNode(agenda[i].Ceremonies + "\n");
+          paragraph.appendChild(text);
+          afficheSport(agenda[i].Ceremonies);
         }
         break;
       case 8:
         if (agenda[i].day8 !== '-') {
-          alert(agenda[i].Ceremonies);
+          var paragraph = newDate.querySelector('.event-list-item-title');
+          var text = document.createTextNode(agenda[i].Ceremonies + "\n");
+          paragraph.appendChild(text);
+          afficheSport(agenda[i].Ceremonies);
         }
         break;
       case 9:
         if (agenda[i].day9 !== '-') {
-          alert(agenda[i].Ceremonies);
+          var paragraph = newDate.querySelector('.event-list-item-title');
+          var text = document.createTextNode(agenda[i].Ceremonies + "\n");
+          paragraph.appendChild(text);
+          afficheSport(agenda[i].Ceremonies);
         }
         break;
       case 10:
         if (agenda[i].day10 !== '-') {
-          alert(agenda[i].Ceremonies);
+          var paragraph = newDate.querySelector('.event-list-item-title');
+          var text = document.createTextNode(agenda[i].Ceremonies + "\n");
+          paragraph.appendChild(text);
         }
         break;
       case 11:
         if (agenda[i].day11 !== '-') {
-          alert(agenda[i].Ceremonies);
+          var paragraph = newDate.querySelector('.event-list-item-title');
+          var text = document.createTextNode(agenda[i].Ceremonies + "\n");
+          paragraph.appendChild(text);
+          afficheSport(agenda[i].Ceremonies);
         }
         break;
       case 12:
         if (agenda[i].day12 !== '-') {
-          alert(agenda[i].Ceremonies);
+          var paragraph = newDate.querySelector('.event-list-item-title');
+          var text = document.createTextNode(agenda[i].Ceremonies + "\n");
+          paragraph.appendChild(text);
+          afficheSport(agenda[i].Ceremonies);
         }
         break;
       case 13:
         if (agenda[i].day13 !== '-') {
-          alert(agenda[i].Ceremonies);
+          var paragraph = newDate.querySelector('.event-list-item-title');
+          var text = document.createTextNode(agenda[i].Ceremonies + "\n");
+          paragraph.appendChild(text);
+          afficheSport(agenda[i].Ceremonies);
         }
         break;
       case 14:
         if (agenda[i].day14 !== '-') {
-          alert(agenda[i].Ceremonies);
+          var paragraph = newDate.querySelector('.event-list-item-title');
+          var text = document.createTextNode(agenda[i].Ceremonies + "\n");
+          paragraph.appendChild(text);
+          afficheSport(agenda[i].Ceremonies);
         }
         break;
       case 18:
         if (agenda[i].day18 !== '-') {
-          alert(agenda[i].Ceremonies);
+          var paragraph = newDate.querySelector('.event-list-item-title');
+          var text = document.createTextNode(agenda[i].Ceremonies + "\n");
+          paragraph.appendChild(text);
+          afficheSport(agenda[i].Ceremonies);
         }
         break;
       case 15:
         if (agenda[i].day15 !== '-') {
-          alert(agenda[i].Ceremonies);
+          var paragraph = newDate.querySelector('.event-list-item-title');
+          var text = document.createTextNode(agenda[i].Ceremonies + "\n");
+          paragraph.appendChild(text);
+          afficheSport(agenda[i].Ceremonies);
         }
         break;
       case 16:
         if (agenda[i].day16 !== '-') {
-          alert(agenda[i].Ceremonies);
+          var paragraph = newDate.querySelector('.event-list-item-title');
+          var text = document.createTextNode(agenda[i].Ceremonies + "\n");
+          paragraph.appendChild(text);
+          afficheSport(agenda[i].Ceremonies);
         }
         break;
       case 17:
         if (agenda[i].day17 !== '-') {
-          alert(agenda[i].Ceremonies);
+          var paragraph = newDate.querySelector('.event-list-item-title');
+          var text = document.createTextNode(agenda[i].Ceremonies + "\n");
+          paragraph.appendChild(text);
+          afficheSport(agenda[i].Ceremonies);
         }
         break;
       case 19:
         if (agenda[i].day19 !== '-') {
-          alert(agenda[i].Ceremonies);
+          var paragraph = newDate.querySelector('.event-list-item-title');
+          var text = document.createTextNode(agenda[i].Ceremonies + "\n");
+          paragraph.appendChild(text);
+          afficheSport(agenda[i].Ceremonies);
         }
         break;
       case 20:
         if (agenda[i].day20 !== '-') {
-          alert(agenda[i].Ceremonies);
+          var paragraph = newDate.querySelector('.event-list-item-title');
+          var text = document.createTextNode(agenda[i].Ceremonies + "\n");
+          paragraph.appendChild(text);
+          afficheSport(agenda[i].Ceremonies);
         }
         break;
 
@@ -178,14 +242,53 @@ function renderCal(day) {
 
 }
 
-// Itère sur toutes les produits
+
+function afficheSport(sport) {
+  const newEvent = eventListItemTemplate.content.cloneNode(true) // true pour cloner également les enfants du node
+  var sportName = sport;
+  var tiretSport = sport.replace("-", ' ');
+  var testSport = " " + tiretSport;
+  //creating a default Array constructor
+  var eventArray = new Set();
+  for (var i = 0; i < medal.length; i++) {
+    if (medal[i].SPORTS == testSport) {
+      eventArray.add(medal[i].EVENT);
+    }
+  }
+  eventlist.append(newEvent)
+  for (let eventName of eventArray) {
+    // Les tags dont nous avons besoin pour afficher les events
+    const sportlist = document.querySelector('.sport-list')
+    const sportListItemTemplate = document.querySelector('#sport-list-item-template')
+    const newSport = sportListItemTemplate.content.cloneNode(true) // true pour cloner également les enfants du node
+    newSport.querySelector('.sportName').innerText = sportName;
+    newSport.querySelector('.eventName').innerText = eventName;
+
+    for (var i = 0; i < medal.length; i++) {
+      if (medal[i].SPORTS == testSport && medal[i].EVENT == eventName) {
+        var paragraph = newSport.querySelector('.medal-list-item-title');
+        var text = document.createTextNode(medal[i].NAME + "--------------------------" + medal[i].NOC + "--------------------------" + medal[i].MEDAL + "\n");
+        paragraph.appendChild(text);
+
+      }
+    }
+
+    sportlist.append(newSport)
+  }
+
+}
+
+function affichesSport() {
+  // On vide la liste
+  eventlist.replaceChildren()
+}
+
 function renderCalSections(day) {
   // On vide la liste
   datlist.replaceChildren()
   renderCal(day)
 }
 
-// Charge les artistes et itère dessus
 function renderCalSection(day) {
   renderCalSections(day)
 }
